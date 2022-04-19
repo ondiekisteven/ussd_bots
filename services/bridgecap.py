cd0dfaad6dd8e3d0d2eda7699e0a688927859b76
Steven <ondiekisteven1@gmail.com>
2022-04-19 16:04:45 +0300

    update bridgecap

--- services/bridgecap.py
+++ services/bridgecap.py
@@ -10,6 +10,7 @@ logger = logging.getLogger(__name__)
 
 def ussd_handler(message: IncomingMessageSchema, session_id, cfg=None):
     if message.body.lower() == 'start':
+
         logger.info("new session starting...")
         body = ''
     else:
@@ -18,9 +19,9 @@ def ussd_handler(message: IncomingMessageSchema, session_id, cfg=None):
     if 'c.us' in message.chatId:
         if '254742976667' in message.chatId:
             print("modifying url...")
-            url = f'http://34.247.24.244:5002/ussd?MSISDN=0712345678&session_id={session_id}&ussd_string={body}'
+            url = f'{cfg["bridgecap"]["remote_url"]}/ussd?MSISDN=0712345678&session_id={session_id}&ussd_string={body}'
         else:
-            url = f'http://34.247.24.244:5002/ussd?MSISDN={message.chatId.replace("@c.us", "")}&session_id={session_id}&ussd_string={body}'
+            url = f'{cfg["bridgecap"]["remote_url"]}/ussd?MSISDN={message.chatId.replace("@c.us", "")}&session_id={session_id}&ussd_string={body}'
         resp = requests.get(url)
         logger.info(f"{resp.status_code}  ::  {resp.text}")
         if resp.status_code == 200:
